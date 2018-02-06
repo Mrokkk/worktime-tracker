489142ffb068783a57397c3158559184235eb6bd
Mrokkk <mrokk@protonmail.com>
2018-02-06 22:00:53 +0100

    Some improvements

--- worktime_tracker.py
+++ worktime_tracker.py
@@ -10,34 +10,28 @@ import datetime
 DB_FILE = os.path.expanduser('~') + '/.worktime'
 REQUIRED_WORKTIME_PER_DAY = 8.0
 
+def convert_minutes(time):
+    hours, minutes = int(time / 60), int(abs(time) % 60)
+    return '{}{:02d}h{:02d}'.format('-' if time < 0 else '', hours, minutes)
+
 def print_summary(full_worktime, days_of_work, delta):
     print('Summary:')
     print('  days of work:', days_of_work)
-    print('  worktime per day:', '{0:.2f}'.format(full_worktime / days_of_work), 'h')
-    print('  worktime delta:', '{0:.2f}'.format(delta), 'h')
+    print('  worktime per day:', '{}'.format(convert_minutes(full_worktime / days_of_work)))
+    print('  worktime delta:', '{}'.format(convert_minutes(delta)))
 
 def print_entry(timestamp, worktime):
-    print(timestamp.strftime('%Y-%m-%d %H:%M:%S'), ': logged', worktime, 'h')
+    print(timestamp.strftime('%Y-%m-%d %H:%M:%S'), ': logged', convert_minutes(worktime))
 
 def show_log(data, show_pre_entry=True):
-    full_worktime, days_of_work, delta = 0, 0, 0
-    last_entry = None
-    last_timestamp = 0.0
-    for entry in data['data']:
-        timestamp = float(entry['timestamp'])
-        date = datetime.datetime.fromtimestamp(timestamp)
-        entry_value = float(entry['worktime'])
+    full_worktime, delta = 0, 0
+    for days_of_work, entry in enumerate(data['data'], start=1):
+        date = datetime.datetime.fromtimestamp(float(entry['timestamp']))
+        entry_value = int(entry['worktime'])
         full_worktime += entry_value
-        old = datetime.datetime.fromtimestamp(last_timestamp)
-        diff = datetime.timedelta(days=date.day) - datetime.timedelta(days=old.day)
-        if diff.days:
-            days_of_work += 1
-            delta += entry_value - REQUIRED_WORKTIME_PER_DAY
-        else:
-            delta += entry_value
+        delta += entry_value - REQUIRED_WORKTIME_PER_DAY * 60
         if (show_pre_entry):
             print_entry(date, entry_value)
-        last_timestamp = timestamp
     print_summary(full_worktime, days_of_work, delta)
 
 def show(args):
@@ -50,10 +44,17 @@ def read_db():
 
 def read_uptime():
     with open('/proc/uptime', 'r') as f:
-        return float(f.readline().split()[0]) / 3600
+        return float(f.readline().split()[0]) / 60
 
 def add_worktime(data, worktime):
-    data["data"].append({'worktime': "{0:.2f}".format(round(worktime, 2)), 'timestamp': str(time.time())})
+    current_day = time.strftime('%x')
+    try:
+        last_day = data['data'][-1]['day']
+        if current_day == last_day:
+            data['data'][-1]['worktime'] += worktime
+            pass
+    except:
+        data["data"].append({'worktime': worktime, 'timestamp': str(time.time()), 'day': current_day})
     with open(DB_FILE, 'w+') as db_file:
         json.dump(data, db_file)
 
@@ -63,7 +64,7 @@ def add(args):
         return
     if args.time:
         splitted = args.time.split('h')
-        time = int(splitted[0]) + round(int(splitted[1]) / 60, 2)
+        time = int(splitted[0]) * 60 + int(splitted[1])
     worktime = read_uptime() if args.uptime else time
     data = read_db() if os.path.exists(DB_FILE) else {"data": []}
     add_worktime(data, worktime)
@@ -79,7 +80,8 @@ def add_add_command(subparsers):
     parser_add.add_argument('-u', '--uptime', action='store_true', help='Log worktime from current uptime')
     parser_add.set_defaults(func=add)
 
-def parse_args():
+def main():
+    os.umask(0o077)
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(help='subcommand help')
     add_show_command(subparsers)
@@ -87,9 +89,5 @@ def parse_args():
     args = parser.parse_args()
     args.func(args)
 
-def main():
-    os.umask(0o077)
-    parse_args()
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
